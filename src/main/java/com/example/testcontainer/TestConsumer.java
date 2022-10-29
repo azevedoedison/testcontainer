7c8577b2a386868d59076e73a291263f12dc636c
f0fp1403 <azevedoedison@gmail.com>
2022-10-29 18:39:45 -0300

    Second commit

--- src/main/java/com/example/testcontainer/TestConsumer.java
+++ src/main/java/com/example/testcontainer/TestConsumer.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestConsumer implements Runnable {
+	
 
     private Consumer<Long, String> consumer;
     private Long pollDuration;
